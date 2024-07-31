1f80f6ec94cda80f1eecc27835c70a57b8e1895c
NatanzMeshaal <natanz_saptoaji_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:13:31 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        String nama ="NatanzMeshaal";
+        String kelas = "X RPL 5";
+        System.out.print("Halo, Nama saya
+        " + nama + " dari kelas " +
+        kelas);
+    }
+}
